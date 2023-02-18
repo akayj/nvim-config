4ca2f4181eb234e0cefa8413804ea6ea3f9c82d0
akayj <askingyj@gmail.com>
2023-02-18 22:40:24 +0800

    dark_colorblind is much better

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,6 @@
 require("github-theme").setup({
   theme_style = "dark_default",
+  -- theme_style = "dark_colorblind",
   -- function_style = "italic",
   sidebars = {"qf", "vista_kind", "terminal", "packer"},
 
