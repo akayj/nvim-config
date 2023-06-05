4686a59653b3c21a94960178fce3ae7101ae4a27
akayj <askingyj@gmail.com>
2023-06-05 15:59:12 +0800

    update

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -11,12 +11,12 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-vim.cmd([[
-augroup packer_user_config
-autocmd!
-" autocmd BufWritePost plugins.lua source <afile> | PackerSync
-augroup end
-]])
+-- vim.cmd([[
+-- augroup packer_user_config
+-- autocmd!
+-- " autocmd BufWritePost plugins.lua source <afile> | PackerSync
+-- augroup end
+-- ]])
 
 return require("packer").startup(function(use)
     use("wbthomason/packer.nvim")
