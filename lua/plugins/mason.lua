c53625224b27f344f3703b7a744ad692b0e6df5c
akayj <askingyj@gmail.com>
2023-06-06 20:51:31 +0800

    update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,9 +1,38 @@
-require("mason").setup({
+local status_ok, mason = pcall(require, "mason")
+if not status_ok then
+    return
+end
+
+local status_ok_1, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not status_ok_1 then
+    print("mason-lspconfig is missing")
+    return
+end
+
+local servers = {
+    "cssls",
+    "html",
+    "jsonls",
+    "tsserver",
+    "yamlls",
+    "bashls",
+    "rust_analyzer",
+    "gopls",
+}
+
+mason.setup({
+    border = "rounded",
     ui = {
         icons = {
             package_installed = "✓",
             package_pending = "➜",
             package_uninstalled = "✗"
         }
-    }
+    },
+    max_concurrent_installers = 4,
 })
+
+mason_lspconfig.setup {
+    ensure_installed = servers,
+    automatic_installation = true,
+}
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,16 +1,42 @@
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls_available, null_ls = pcall(require, "null-ls")
+if not null_ls_available then
+    print("null-ls is not available")
+    return
+end
 
-require("null-ls").setup({
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ async = false })
-                end,
-            })
-        end
-    end,
-})
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup {
+    debug = false,
+    sources = {
+        formatting.prettier.with {
+            extra_filetypes = { "toml", "solidity" },
+            extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+        },
+        formatting.black.with { extra_args = {"--fast"} },
+        formatting.stylua,
+        formatting.shfmt,
+        formatting.rustfmt,
+        formatting.google_java_format,
+
+        diagnostics.shellcheck,
+    },
+}
+
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+--
+-- null_ls.setup({
+--     on_attach = function(client, bufnr)
+--         if client.supports_method("textDocument/formatting") then
+--             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--             vim.api.nvim_create_autocmd("BufWritePre", {
+--                 group = augroup,
+--                 buffer = bufnr,
+--                 callback = function()
+--                     vim.lsp.buf.format({ async = false })
+--                 end,
+--             })
+--         end
+--     end,
+-- })
