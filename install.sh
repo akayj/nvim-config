8f04dc37b0c716ff62a0c0d58a738f4d9a4d9f3d
akayj <askingyj@gmail.com>
2023-09-13 14:31:06 +0800

    do not curl at startup time

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o pipefail
-set -o nounset
+set -euo pipefail
 
-readonly RELEASE_URL='https://github.com/neovim/neovim/releases/download/nightly/nvim-%s.%s'
+readonly RELEASE_URL='https://github.com/neovim/neovim/releases/download/nightly/'
 
-# [ -f "./lib/colors.sh" ] && source "./lib/colors.sh"
-readonly WHITE='37'     # white
-readonly Red='31'       # Red
+readonly WHITE='37' # white
+# readonly Red='31'       # Red
 readonly Purple='35'    # Purple
 readonly BRed='1;31'    # Red
 readonly BGreen='32'    # Green
@@ -16,84 +13,87 @@ readonly BYellow='0;33' # Yellow
 readonly BBlue='1;34'   # Blue
 
 function expand_color() {
-	local colors="${1:-${WHITE}}"
+  local colors="${1:-${WHITE}}"
 
-	echo "${colors}" | awk -F ';' '{ if ($2 == "") print 0";"$1; else print $1";"$2; }'
+  echo "${colors}" | awk -F ';' '{ if ($2 == "") print 0";"$1; else print $1";"$2; }'
 }
 
 function color_message() {
-	local color
-	color=$(expand_color "$1")
-	shift
+  local color
+  color=$(expand_color "$1")
+  shift
 
-	local template="$1"
-	shift
+  local template="$1"
+  shift
 
-	printf "\e[%sm$template\e[0m" "${color}" "$@"
+  printf "\e[%sm$template\e[0m" "${color}" "$@"
 }
 
 function log::log() { color_message '' "$@"; }
 
 # log::debug() { color_message '35' "$@"; }
 function log::debug() {
-	color_message "$BBlue" "$@"
-	echo
+  color_message "$BBlue" "$@"
+  echo
 }
 function log::success() {
-	color_message "$BGreen" "$@"
-	echo
+  color_message "$BGreen" "$@"
+  echo
 }
 # shellcheck disable=SC2317
 function log::warn() {
-	color_message "$BYellow" "$@"
-	echo
+  color_message "$BYellow" "$@"
+  echo
 }
+
 function log::info() {
-	color_message "$Purple" "$@"
-	echo
+  color_message "$Purple" "$@"
+  echo
 }
+
 # shellcheck disable=SC2317
 function log::error() {
-	color_message "$BRed" "$@" >&2
-	echo
+  color_message "$BRed" "$@" >&2
+  echo
 }
 
 function log::fatal() {
-	local message=${1:-}
-	local exit_code=${2:-1}
+  local message=${1:-}
+  local exit_code=${2:-1}
 
-	[[ -z ${1-} ]] || {
-		log::error "$message" >&2
-	}
+  [[ -z ${1-} ]] || {
+    log::error "$message" >&2
+  }
 
-	exit "$exit_code"
+  exit "$exit_code"
 }
 
 declare curl_version
-curl_version="$(curl -V | head -n 1 | cut -d ' ' -f 2)"
-readonly curl_version
+
+function set_curl_version() {
+  if [ -z "${curl_version}" ]; then
+    curl_version="$(curl -V | head -n 1 | cut -d ' ' -f 2)"
+    readonly curl_version
+  fi
+}
 
 function https_curl() {
-	if [[ $(echo "${curl_version} 7.20.0" | awk '{print ($1 > $2)}') -eq 1 ]]; then
-		curl --proto '=https' --tlsv1.2 "$@"
-	else
-		curl --tlsv1.2 "$@"
-	fi
+  if [[ $(echo "${curl_version} 7.20.0" | awk '{print ($1 > $2)}') -eq 1 ]]; then
+    curl --proto '=https' --tlsv1.2 "$@"
+  else
+    curl --tlsv1.2 "$@"
+  fi
 }
 
-readonly https_curl
+function retrive_file() {
+  local url="${1:-}"
 
-function download_url() {
-	local url="${1:-}"
+  if [ "${url}" == "" ]; then
+    return 1
+  fi
 
-	if [ "${url}" == "" ]; then
-		return 1
-	fi
-
-	https_curl --progress-bar -fLO "${url}"
+  https_curl --progress-bar -fLO "${url}"
 }
-
-readonly download_url
 
 function nvim_version() {
   local home_dir="${1:-}"
@@ -104,67 +104,71 @@ function nvim_version() {
     nvim_bin="${home_dir}/bin/nvim"
   fi
 
-	if command -v "${nvim_bin}" > /dev/null || [ -x "${nvim_bin}" ]; then
-		"${nvim_bin}" -v | head -n 1 | awk '{print $2}'
-	fi
+  if command -v "${nvim_bin}" >/dev/null || [ -x "${nvim_bin}" ]; then
+    "${nvim_bin}" -v | head -n 1 | awk '{print $2}'
+  fi
 }
 
 function check_file() {
-	local filename="${1:-}"
-	if [ -z "${filename}" ]; then
-		log::error "empty filename is invalid"
-		return 1
-	fi
+  local filename="${1:-}"
+  if [ -z "${filename}" ]; then
+    log::error "empty filename is invalid"
+    return 1
+  fi
 
-	local sumfile="${2:-${filename}.sha256sum}"
-	if [ ! -f "${sumfile}" ]; then
-		return 1
-	fi
+  local sumfile="${2:-${filename}.sha256sum}"
+  if [ ! -f "${sumfile}" ]; then
+    return 1
+  fi
 
-	if command -v sha256sum >/dev/null; then
-		sha256sum -c "${sumfile}" >/dev/null 2>&1
-	elif command -v shasum >/dev/null; then
-		shasum -c "${sumfile}" >/dev/null 2>&1
-	else
-		return 1
-	fi
+  if command -v sha256sum >/dev/null; then
+    sha256sum -c "${sumfile}" >/dev/null 2>&1
+  elif command -v shasum >/dev/null; then
+    shasum -c "${sumfile}" >/dev/null 2>&1
+  else
+    return 1
+  fi
 }
 
 function download_and_check() {
-	local url="${1:-}"
-	local check="${2:-y}"
+  local url="${1:-}"
+  local check="${2:-y}"
 
-	if [ -z "${url}" ]; then
-		log::error 'empty url'
-		return 1
-	fi
+  if [ -z "${url}" ]; then
+    log::error 'empty url'
+    return 1
+  fi
 
-	local filename=$(basename "${url}")
+  local filename
+  filename=$(basename "${url}")
 
   if [ ! -f "${filename}" ]; then
     log::log 'downlading %s...' "${filename}"
-    download_url "${url}" > /dev/null || return 1
+    retrive_file "${url}" >/dev/null || return 1
     log::success 'OK'
   else
-    read -p 'Found local tarball, use it [Y/n]: ' -s -n1 Yn
+    read -rp 'Found local tarball, use it [Y/n]: ' -s -n1 Yn
     case "${Yn}" in
-      [Nn]*)
-        log::error "No"
-        log::log 'download new tarbar...'
-        download_url "${url}" > /dev/null 2>&1 || { log::error 'Fail'; return 2; }
-        log::success 'OK'
-        ;;
+    [Nn]*)
+      log::error "No"
+      log::log 'download new tarbar...'
+      retrive_file "${url}" >/dev/null 2>&1 || {
+        log::error 'Fail'
+        return 2
+      }
+      log::success 'OK'
+      ;;
 
-      *)
-        log::success "Yes"
-        log::info 'Use local tarball'
-        ;;
+    *)
+      log::success "Yes"
+      log::info 'Use local tarball'
+      ;;
     esac
   fi
 
   if [ "${check}" == 'y' ]; then
     log::log 'Checking sha256sum...'
-    download_url "${url}.sha256sum" > /dev/null 2>&1 || return 1
+    retrive_file "${url}.sha256sum" >/dev/null 2>&1 || return 1
     if check_file "${filename}" >/dev/null; then
       log::success 'OK'
       return
@@ -174,104 +178,100 @@ function download_and_check() {
   fi
 }
 
-readonly download_and_check
-
 function relink() {
-	local top_dir="${1}"
-	local nvim_home="${2:-${HOME}}"
+  local top_dir="${1}"
+  local nvim_home="${2:-${HOME}}"
   local old_version
 
-	if [ -z "${top_dir}" ]; then
-		log::error 'top_dir is required'
-		return 1
-	fi
+  if [ -z "${top_dir}" ]; then
+    log::error 'top_dir is required'
+    return 1
+  fi
 
   old_version=$(nvim_version "${nvim_home}/nvim_home")
-	if [ -d "${nvim_home}/${top_dir}" ]; then
-		rm -rf "${nvim_home:?}/${top_dir}" || log::fatal 'remove old dir failed'
-	fi
+  if [ -d "${nvim_home}/${top_dir}" ]; then
+    rm -rf "${nvim_home:?}/${top_dir}" || log::fatal 'remove old dir failed'
+  fi
 
-	log::log 'untar...'
-	tar -C "${nvim_home}" -xf "${top_dir}.tar.gz" >/dev/null
-	log::success 'done'
+  log::log 'untar...'
+  tar -C "${nvim_home}" -xf "${top_dir}.tar.gz" >/dev/null
+  log::success 'done'
 
-	log::log 'linking...'
-	ln -s -f "${nvim_home}/${top_dir}" "${nvim_home}/nvim_home"
-	log::success 'done'
+  log::log 'linking...'
+  ln -s -f "${nvim_home}/${top_dir}" "${nvim_home}/nvim_home"
+  log::success 'done'
 
-	local new_version=$(nvim_version "${nvim_home}/nvim_home")
-	if [ -z "${old_version}" ]; then
-		log::success "new nvim %s installed" "${new_version}"
-	elif [ "${old_version}" != "${new_version}" ]; then
-		log::success "nvim upgraded, %s -> %s" "${old_version}" "${new_version}"
-	else
+  local new_version=$(nvim_version "${nvim_home}/nvim_home")
+  if [ -z "${old_version}" ]; then
+    log::success "new nvim %s installed" "${new_version}"
+  elif [ "${old_version}" != "${new_version}" ]; then
+    log::success "nvim upgraded, %s -> %s" "${old_version}" "${new_version}"
+  else
     log::success "nvim %s reinstalled" "${new_version}"
-	fi
+  fi
 }
 
-readonly relink
-
 function current_shell_rc() {
-	local current_shell
-	current_shell=$(basename "$SHELL")
+  local current_shell
+  current_shell=$(basename "$SHELL")
 
-	case ${current_shell} in
-	"zsh")
-		echo "${HOME}/.zshrc"
-		;;
+  case ${current_shell} in
+  "zsh")
+    echo "${HOME}/.zshrc"
+    ;;
 
-	"bash")
-		echo "${HOME}/.bashrc"
-		;;
-	esac
+  "bash")
+    echo "${HOME}/.bashrc"
+    ;;
+  esac
 }
 
 function install_nvim() {
-	local force="${1:-}"
-	local old_version=$(nvim_version)
+  local force="${1:-}"
+  local old_version
 
-	if [ "${old_version}" != "" ]; then
-		log::info "nvim %s was installed" "${old_version}"
-		if [ "${force}" != "-f" ]; then
-			return
-		else
-			log::warn 'force to install'
-		fi
-	fi
+  old_version=$(nvim_version)
 
-	local os_name=$(uname -s | tr '[:upper:]' '[:lower:]')
-	local file_url=''
-	local checksum_url=''
+  if [ "${old_version}" != "" ]; then
+    log::info "nvim %s was installed" "${old_version}"
+    if [ "${force}" != "-f" ]; then
+      return
+    else
+      log::warn 'force to install'
+    fi
+  fi
 
-	case "${os_name}" in
-	"darwin")
-		file_url=$(printf "${RELEASE_URL}" "macos" "tar.gz")
-		checksum_url="${file_url}.sha256sum"
+  set_curl_version
 
-		download_and_check "${file_url}" || exit 1
-		xattr -c ./nvim-macos.tar.gz && relink 'nvim-macos'
-		;;
+  local os_name=$(uname -s | tr '[:upper:]' '[:lower:]')
+  local file_url
+  # local checksum_url=''
 
-	"linux")
-		file_url=$(printf "${RELEASE_URL}" "linux64" "tar.gz")
-		checksum_url="${file_url}.sha256sum"
+  case "${os_name}" in
+  "darwin")
+    file_url=$(printf "${RELEASE_URL}/nvim-%s.%s" "macos" "tar.gz")
 
-		download_and_check "${file_url}" || exit 1
-		relink 'nvim-linux64'
-		;;
+    download_and_check "${file_url}" || exit 1
+    xattr -c ./nvim-macos.tar.gz && relink 'nvim-macos'
+    ;;
 
-	*)
-		log::error "unknown os ${os_name}"
-		return 1
-		;;
-	esac
+  "linux")
+    file_url=$(printf "${RELEASE_URL}/nvim-%s.%s" "linux64" "tar.gz")
+
+    download_and_check "${file_url}" || exit 1
+    relink 'nvim-linux64'
+    ;;
+
+  *)
+    log::error "unknown os ${os_name}"
+    return 1
+    ;;
+  esac
 }
 
-readonly install_nvim
-
 [ -d "${HOME}/.config/nvim" ] || {
-	git clone https://github.com/NvChad/NvChad.git ~/.config/nvim --depth=1
-	git clone https://github.com/akayj/nvim-config.git ~/.config/nvim/custom --depth=1
+  git clone https://github.com/NvChad/NvChad.git ~/.config/nvim --depth=1
+  git clone https://github.com/akayj/nvim-config.git ~/.config/nvim/custom --depth=1
 }
 
 install_nvim "$@" || log::fatal "failed"
